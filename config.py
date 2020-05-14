8a96cb4145d2bc2e702f75f857bec75779b0e0cd
Sohrab Ahmadpur <sahmadpur@kibrit.tech>
2020-05-14 17:54:09 +0400

    first commit

---
+++ config.py
@@ -0,0 +1,48 @@
+
+
+#Bot token
+TOKEN = "900555854:AAGl6LXOB_ID82M8bFepSV7eUnGbzMI0Lk8" #testbot token
+
+#Messages
+
+Lang_chose_az = "Zəhmət olmasa axtarış dilini seçin!"
+lang_not_chsn = "Atarış dili seçilməyib! Zəhmət olmasa /lang komandası ilə axtarış dilini seçin!"
+no_start = "Zəhmət olmasa ilk öncə /start komandasından istifadə edin!"
+
+
+none_text = {
+    "none_az" : "Mən yalnız sözləri axtara bilirəm.",
+    "none_ru" : "Я могу искать только слова.",
+    "none_en" : "I can only search for words.",
+    "none_scr_id" : "CAACAgIAAxkBAAILTF5FEjDaUtAO2n0qlhh9ZfDkcv_oAAJFAAMh8AQcVEccChUEGqEYBA"
+    }
+
+
+
+Query_handler = {
+    "hello_msg" : "Bildilçin, Sizin üçün axtarar!" ,
+    "fa_az" : "Axtarmaq istədiyiniz sözü daxil edin:" ,
+    "fa_ru" : "Введите слово, которое вы хотите найти:" ,
+    "fa_en" : "Enter the word you want to find:" ,
+    "fa_" : lang_not_chsn
+}
+
+
+No_Word = {
+    "no_az" : "Axtardığınız söz tapılmadı! Yeni sözü daxil edin və ya axtarış dilini dəyişmək üçün /lang komandasından istifadə edin!",
+    "no_ru" : "Слово, которое вы ищете, не найдено! Введите новое слово или используйте команду /lang чтобы изменить язык поиска.",
+    "no_en" : "The word you are looking for not found! Enter a new word or use the /lang command to change the language."
+    }
+
+Yes_Word = {
+    "yes_az" : "Axtarış nəticələri:",
+    "yes_ru" : "Результаты поиска для:",
+    "yes_en" : "Search results for:"
+    }
+
+
+New_Word = {
+    "new_az" : "Yeni sözü daxil edin və ya axtarış dilini dəyişmək üçün /lang komandasından istifadə edin!" ,
+    "new_ru" : "Введите новое слово или используйте команду /lang чтобы изменить язык поиска!" ,
+    "new_en" : "Enter a new word or use the /lang command to change the search language!"
+    }
---
+++ main.py
@@ -0,0 +1,136 @@
+import os
+from flask import Flask, request
+import telebot
+import config
+import search_parser as sp
+from search_parser import word_info, word_list
+import time
+import logging
+
+
+knownUsers = []
+userStep = {}
+
+
+
+bot = telebot.TeleBot(config.TOKEN)
+server = Flask(__name__)
+logging.basicConfig(level=logging.INFO, filename="sys.log")
+
+
+@bot.message_handler(commands=['start'])
+def start_message(m):
+    cid = m.chat.id
+    markup = telebot.types.InlineKeyboardMarkup()
+    markup.add(telebot.types.InlineKeyboardButton(text='Azərbaycan', callback_data="az"))
+    markup.add(telebot.types.InlineKeyboardButton(text='Русский', callback_data="ru"))
+    markup.add(telebot.types.InlineKeyboardButton(text='English', callback_data="en"))
+    if cid not in knownUsers:
+        knownUsers.append(cid)
+        userStep[cid] = ""
+        bot.send_message(cid, text=config.Lang_chose_az, reply_markup=markup)
+    else:
+        bot.send_message(cid, text=config.Query_handler["fa_{}".format(userStep[cid])])
+
+
+@bot.message_handler(commands=["lang"])
+def change_lang(m):
+    cid = m.chat.id
+    markup = telebot.types.InlineKeyboardMarkup()
+    markup.add(telebot.types.InlineKeyboardButton(text='Azərbaycan', callback_data="az"))
+    markup.add(telebot.types.InlineKeyboardButton(text='Русский', callback_data="ru"))
+    markup.add(telebot.types.InlineKeyboardButton(text='English', callback_data="en"))
+    if cid not in knownUsers:
+        knownUsers.append(cid)
+        bot.send_message(cid, text=config.Lang_chose_az, reply_markup=markup)
+    else:
+        bot.send_message(cid, text=config.Lang_chose_az, reply_markup=markup)
+
+
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def query_handler(call):
+    bot.answer_callback_query(callback_query_id=call.id, text=config.Query_handler["hello_msg"])
+    first_answer = ''
+    if call.data == 'az':
+        cd = call.data
+        userStep.update({call.message.chat.id: cd})
+        first_answer = config.Query_handler["fa_az"]
+    elif call.data == 'ru':
+        cd = call.data
+        userStep.update({call.message.chat.id: cd})
+        first_answer = config.Query_handler["fa_ru"]
+    elif call.data == 'en':
+        cd = call.data
+        userStep.update({call.message.chat.id: cd})
+        first_answer = config.Query_handler["fa_en"]
+    bot.send_message(call.message.chat.id, first_answer)
+
+
+
+@bot.message_handler(func=lambda message: True)
+def word_search(message):
+    cid = message.chat.id
+    if cid not in knownUsers:
+        knownUsers.append(cid)
+        userStep.update({cid:""})
+        bot.send_message(message.chat.id, text=config.lang_not_chsn)
+        #"New user detected, who hasn't used \"/start\" yet"
+    else:
+        cd = userStep[cid]
+        if str(message.text) == "None":
+            bot.send_sticker(message.chat.id, config.none_text["none_scr_id"])
+            bot.send_message(message.chat.id, text= config.none_text[("none_{}".format(cd))])
+        else:
+            second_answer = sp.dict_search(word=message.text, lang= cd)
+            if second_answer == "Word_Not_Found":
+                second_answer = config.No_Word[("no_{}".format(cd))]
+                bot.send_message(message.chat.id, second_answer)
+            else:
+                bot.send_message(message.chat.id, "{0} {1}".format(config.Yes_Word["yes_{}".format(cd)] , message.text))
+                for element in second_answer:
+                    bot.send_message(message.chat.id, "<b>" + element.dict_name + "</b>", parse_mode="HTML")
+                    if len(element.w_info) > 4096:
+                        for x in range(0, len(element.w_info), 4096):
+                            bot.send_message(message.chat.id, element.w_info[x:x + 4096])
+                    else:
+                        bot.send_message(message.chat.id, element.w_info)
+                bot.send_message(message.chat.id, text= config.New_Word[("new_{}".format(cd))])
+                word_list.clear()
+                cd = ""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+@server.route('/' + config.TOKEN, methods=['POST'])
+def getMessage():
+    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "!", 200
+
+
+@server.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url='https://bilbotwebhooked.herokuapp.com/' + config.TOKEN)
+    return "!", 200
+
+
+if __name__ == "__main__":
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
